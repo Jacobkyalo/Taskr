95718d0a751823bacf97b64984202b4a9acb4eab
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-02-03 11:24:48 +0300

    updated env vars

--- lib/appwrite.config.ts
+++ lib/appwrite.config.ts
@@ -1,14 +1,16 @@
+// "use server";
+
 import { Client, Account, Databases } from "appwrite";
 
 const client = new Client();
 
 client
-  .setEndpoint(process.env.API_ENDPOINT as string)
-  .setProject(process.env.PROJECT_ID as string);
+  .setEndpoint(process.env.NEXT_PUBLIC_API_ENDPOINT as string)
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID as string);
 
 export const account = new Account(client);
 export const database = new Databases(client);
-export const COLLECTION_ID = process.env.COLLECTION_ID as string;
-export const DB_ID = process.env.DB_ID as string;
+export const COLLECTION_ID = process.env.NEXT_PUBLIC_COLLECTION_ID as string;
+export const DB_ID = process.env.NEXT_PUBLIC_DB_ID as string;
 
 // export default client;
