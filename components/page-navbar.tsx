5becb467610d26069f7aa5e161b86c341c821505
Jacob Kyalo <jacobkyalo43@gmail.com>
2024-02-27 16:42:39 +0300

    added theme toggler btn

--- components/page-navbar.tsx
+++ components/page-navbar.tsx
@@ -2,6 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { ThemeToggler } from "./theme-toggler";
 
 export default function PageNavbar() {
   const pathname = usePathname();
@@ -19,20 +20,23 @@ export default function PageNavbar() {
             <span>Taskr</span>
           </Link>
 
-          <nav>
-            <ul className="flex items-center gap-x-8">
-              <li>
-                <Link href="/signup" className="hover:text-destructive">
-                  Get Started
-                </Link>
-              </li>
-              <li>
-                <Link href="/login" className="hover:text-destructive">
-                  Login
-                </Link>
-              </li>
-            </ul>
-          </nav>
+          <div className="flex items-center gap-x-4 sm:gap-x-8">
+            <nav>
+              <ul className="flex items-center gap-x-4 sm:gap-x-8">
+                <li>
+                  <Link href="/signup" className="hover:text-destructive">
+                    Get Started
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/login" className="hover:text-destructive">
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+            <ThemeToggler />
+          </div>
         </header>
       )}
     </>
